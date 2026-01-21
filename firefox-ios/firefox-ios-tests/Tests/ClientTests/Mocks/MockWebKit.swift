8cfca8804346b80862ccad28963ef74db98aefef
ofir-frd <ofir.f@qodo.ai>
2026-01-21 10:37:27 +0200

    update pr

--- firefox-ios/firefox-ios-tests/Tests/ClientTests/Mocks/MockWebKit.swift
+++ firefox-ios/firefox-ios-tests/Tests/ClientTests/Mocks/MockWebKit.swift
@@ -99,7 +99,7 @@ class MockWKScriptMessage: WKScriptMessage {
     let overridenName: String
     let overridenFrameInfo: WKFrameInfo
 
-    init(name: String, body: Any, frameInfo: WKFrameInfo) {
+    init(scriptMessageHandlerName name: String, messageBody body: Any, webKitFrameInfo frameInfo: WKFrameInfo) {
         overridenBody = body
         overridenName = name
         overridenFrameInfo = frameInfo
